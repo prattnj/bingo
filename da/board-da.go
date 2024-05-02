8404c923ad35ecc5db4e5dc0c6890723ff130c95
Noah Pratt <prattnj@gmail.com>
2024-05-02 16:49:38 -0600

    Stub out data access methods

---
+++ da/board-da.go
@@ -0,0 +1,26 @@
+package da
+
+import (
+	"bingo/model"
+	"database/sql"
+)
+
+func InsertBoard(board model.BoardBean, db *sql.DB) {
+
+}
+
+func UpdateBoard(board model.BoardBean, db *sql.DB) {
+
+}
+
+func GetBoard(id string, db *sql.DB) {
+
+}
+
+func GetBoardsForUser(username string, db *sql.DB) {
+
+}
+
+func GetPublicBoards(db *sql.DB) {
+
+}
---
+++ da/user-da.go
@@ -0,0 +1,22 @@
+package da
+
+import (
+	"bingo/model"
+	"database/sql"
+)
+
+func InsertUser(user model.UserBean, db *sql.DB) {
+
+}
+
+func UpdateUser(user model.UserBean, db *sql.DB) {
+
+}
+
+func GetUser(username string, db *sql.DB) {
+
+}
+
+func ValidatePassword(username string, password string, db *sql.DB) {
+
+}
