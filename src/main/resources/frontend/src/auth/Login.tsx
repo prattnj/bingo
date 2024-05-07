719488bbda4eb963d075e179e4bfaa812ef95df5
Noah Pratt <prattnj@gmail.com>
2024-05-06 18:28:03 -0600

    Add everything except server actions to auth pages

--- src/main/resources/frontend/src/auth/Login.tsx
+++ src/main/resources/frontend/src/auth/Login.tsx
@@ -1,12 +1,43 @@
 import {Link} from "react-router-dom";
 import './auth.css'
+import {useState} from "react";
+import {login} from "./actions";
 
 export const Login = () => {
+    const [username, setUsername] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [errorMessage, setErrorMessage] = useState<string>('');
+
+    const validateFields = () => {
+        if (!username || !password) {
+            setErrorMessage('All fields are required.');
+            return false;
+        }
+        setErrorMessage('');
+        return true;
+    };
+
+    const validateAndLogin = () => {
+        if (!validateFields()) return;
+        login(username, password);
+    };
+
     return (
         <div className={'auth-all'}>
-            <input type={'text'} placeholder={'Username'} className={'input-box'}/>
-            <input type={'text'} placeholder={'Password'} className={'input-box'}/>
-            <div className={'button'}><b>LOGIN</b></div>
+            <input
+                type={'text'}
+                placeholder={'Username'}
+                className={'input-box'}
+                onChange={(event) => setUsername(event.target.value)}
+            />
+            <input
+                type={'password'}
+                placeholder={'Password'}
+                className={'input-box'}
+                onChange={(event) => setPassword(event.target.value)}
+            />
+            <div className={'error'}>{errorMessage}</div>
+            <div className={'button'} onClick={validateAndLogin}><b>LOGIN</b></div>
             <p>New user? <Link to={'/register'}><u>Register</u></Link> instead.</p>
         </div>
     );
--- src/main/resources/frontend/src/auth/Register.tsx
+++ src/main/resources/frontend/src/auth/Register.tsx
@@ -1,11 +1,59 @@
+import {register} from "./actions";
+import {useState} from "react";
+
 export const Register = () => {
+    const [email, setEmail] = useState<string>('');
+    const [username, setUsername] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [repeatPassword, setRepeatPassword] = useState<string>('');
+    const [errorMessage, setErrorMessage] = useState<string>('');
+
+    const validateFields = () => {
+        if (!username || !password || !repeatPassword || !email) {
+            setErrorMessage('All fields are required.');
+            return false;
+        }
+        if (repeatPassword !== password) {
+            setErrorMessage('Passwords don\'t match.');
+            return false;
+        }
+        setErrorMessage('');
+        return true;
+    };
+
+    const validateAndRegister = () => {
+        if (!validateFields()) return;
+        register(username, password, email);
+    };
+
     return (
         <div className={'auth-all'}>
-            <input type={'text'} placeholder={'Email'} className={'input-box'}/>
-            <input type={'text'} placeholder={'Username'} className={'input-box'}/>
-            <input type={'text'} placeholder={'Password'} className={'input-box'}/>
-            <input type={'text'} placeholder={'Repeat Password'} className={'input-box'}/>
-            <div className={'button'}><b>REGISTER</b></div>
+            <input
+                type={'text'}
+                placeholder={'Email'}
+                className={'input-box'}
+                onChange={(event) => setEmail(event.target.value)}
+            />
+            <input
+                type={'text'}
+                placeholder={'Username'}
+                className={'input-box'}
+                onChange={(event) => setUsername(event.target.value)}
+            />
+            <input
+                type={'password'}
+                placeholder={'Password'}
+                className={'input-box'}
+                onChange={(event) => setPassword(event.target.value)}
+            />
+            <input
+                type={'password'}
+                placeholder={'Repeat Password'}
+                className={'input-box'}
+                onChange={(event) => setRepeatPassword(event.target.value)}
+            />
+            <div className={'error'}>{errorMessage}</div>
+            <div className={'button'} onClick={validateAndRegister}><b>REGISTER</b></div>
         </div>
     );
 }
