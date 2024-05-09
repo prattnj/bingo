e870a4912ba8e5b8c1a9c3b087cb72d9e3fac1b6
Noah Pratt <prattnj@gmail.com>
2024-05-09 17:22:34 -0600

    Add title to item

--- src/main/java/model/bean/Item.java
+++ src/main/java/model/bean/Item.java
@@ -2,14 +2,20 @@ package model.bean;
 
 public class Item {
 
+    private final String title;
     private final String description;
     private final boolean checked;
     private final long checkedAt;
 
-    public Item(String description, boolean checked, long checkedAt) {
+    public Item(String title, String description, boolean checked, long checkedAt) {
+        this.title = title;
         this.description = description;
         this.checked = checked;
         this.checkedAt = checkedAt;
+    }
+
+    public String getTitle() {
+        return title;
     }
 
     public String getDescription() {
