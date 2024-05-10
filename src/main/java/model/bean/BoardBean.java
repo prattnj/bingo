f6e758a00e85844a2f3e035ec8725059e7660fc6
Noah Pratt <prattnj@gmail.com>
2024-05-09 18:31:08 -0600

    Convert beans to records

--- src/main/java/model/bean/BoardBean.java
+++ src/main/java/model/bean/BoardBean.java
@@ -1,44 +1,4 @@
 package model.bean;
 
-public class BoardBean {
-
-    private final String id;
-    private final String name;
-    private final String username;
-    private final Item[][] items;
-    private final Long createdAt;
-    private final Boolean isPublic;
-
-    public BoardBean(String id, String name, String username, Item[][] items, Long createdAt, Boolean isPublic) {
-        this.id = id;
-        this.name = name;
-        this.username = username;
-        this.items = items;
-        this.createdAt = createdAt;
-        this.isPublic = isPublic;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public Item[][] getItems() {
-        return items;
-    }
-
-    public Long getCreatedAt() {
-        return createdAt;
-    }
-
-    public Boolean isPublic() {
-        return isPublic;
-    }
+public record BoardBean(String id, String name, String username, Item[][] items, Long createdAt, Boolean isPublic) {
 }
--- src/main/java/model/bean/UserBean.java
+++ src/main/java/model/bean/UserBean.java
@@ -1,36 +1,4 @@
 package model.bean;
 
-public class UserBean {
-
-    private final String username;
-    private final String password;
-    private final String email;
-    private String token;
-
-    public UserBean(String username, String password, String email, String token) {
-        this.username = username;
-        this.password = password;
-        this.email = email;
-        this.token = token;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
+public record UserBean(String username, String password, String email, String token) {
 }
