276d3c4a40763b6a87e16eef7e7fa06df67f7441
Noah Pratt <prattnj@gmail.com>
2024-05-09 18:07:32 -0600

    Add basic BoardDAO functionality

--- src/main/java/da/BoardDAO.java
+++ src/main/java/da/BoardDAO.java
@@ -1,4 +1,82 @@
 package da;
 
+import com.google.gson.Gson;
+import model.bean.BoardBean;
+import model.bean.Item;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.HashSet;
+
 public class BoardDAO {
+
+    public static void insert(BoardBean board) throws SQLException {
+        String query = "INSERT INTO board (id, name, username, items, created_at, public) VALUES (?, ?, ?, ?, ?, ?)";
+        try (Connection conn = Database.getConnection()) {
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, board.getId());
+            ps.setString(2, board.getUsername());
+            ps.setString(3, new Gson().toJson(board.getItems()));
+            ps.setLong(4, board.getCreatedAt());
+            ps.setBoolean(5, board.isPublic());
+            ps.executeUpdate();
+        }
+    }
+
+    public static void update(BoardBean board) throws SQLException {
+        String query = "UPDATE board SET name = ?, items = ?, public = ? WHERE id = ?";
+        try (Connection conn = Database.getConnection()) {
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, board.getName());
+            ps.setString(2, new Gson().toJson(board.getItems()));
+            ps.setBoolean(3, board.isPublic());
+            ps.setString(4, board.getId());
+            ps.executeUpdate();
+        }
+    }
+
+    public static BoardBean get(String id) throws SQLException {
+        String query = "SELECT * FROM board WHERE id = ?";
+        try (Connection conn = Database.getConnection()) {
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) return getBoardFromResultSet(rs);
+            else return null;
+        }
+    }
+
+    public static Collection<BoardBean> getAllUser(String username) throws SQLException {
+        String query = "SELECT * FROM board WHERE username = ?";
+        return getMultipleBoards(query, username);
+    }
+
+    public static Collection<BoardBean> getAllPublic() throws SQLException {
+        String query = "SELECT * FROM board WHERE public = ?";
+        return getMultipleBoards(query, "1");
+    }
+
+    private static Collection<BoardBean> getMultipleBoards(String query, String key) throws SQLException {
+        Collection<BoardBean> boards = new HashSet<>();
+        try (Connection conn = Database.getConnection()) {
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, key);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) boards.add(getBoardFromResultSet(rs));
+        }
+        return boards;
+    }
+
+    private static BoardBean getBoardFromResultSet(ResultSet rs) throws SQLException {
+        String id = rs.getString(1);
+        String name = rs.getString(2);
+        String username = rs.getString(3);
+        Item[][] items = new Gson().fromJson(rs.getString(4), Item[][].class);
+        long createdAt = rs.getLong(5);
+        boolean isPublic = rs.getBoolean(6);
+        return new BoardBean(id, name, username, items, createdAt, isPublic);
+    }
 }
